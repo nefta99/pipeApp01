6a6766b4ef0d5b5224e8140198a0eac457a19ade
nefta99 <israelneftali@gmail.com>
2021-08-04 12:03:55 -0500

    video : 144

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent{
 
-  constructor() { }
+  ventasNetas : number = 25698239.5587;
+  porcentaje : number = 0.48;
 
-  ngOnInit(): void {
-  }
+ 
 
+ 
 }
