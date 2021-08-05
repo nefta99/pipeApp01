4b04b8bdefea5853e0571ac9678fe7639ec741af
nefta99 <israelneftali@gmail.com>
2021-08-05 11:45:04 -0500

    video : 147

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  nombre : string ='israel';
+  genero : string  = 'masculino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino' : 'invitarlo',
+    'femenino' : 'invitarla'
   }
+
+  
 
 }
