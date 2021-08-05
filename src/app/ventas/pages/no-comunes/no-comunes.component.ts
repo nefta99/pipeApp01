59d752929796146cdddacc9974d199df24e48bd9
nefta99 <israelneftali@gmail.com>
2021-08-05 12:34:47 -0500

    video :  148

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,22 @@ import { Component} from '@angular/core';
 })
 export class NoComunesComponent {
 
+  //i18nSelect
   nombre : string ='israel';
   genero : string  = 'masculino';
-
   invitacionMapa = {
     'masculino' : 'invitarlo',
     'femenino' : 'invitarla'
+  }
+
+  //i18nPlural
+  clientes : string []= ['jaun','miguel','challo'];
+
+  clientesMapa ={
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1' : 'tenemos un cliente esperando',
+    '=2' :  'tenemos dos clientes esperando',
+    'other': 'tenemos # clientes esperando'
   }
 
   
